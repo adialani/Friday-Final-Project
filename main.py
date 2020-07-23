0a340cfafe3c76639021d8f7e7b35c099ea67d6d
adialani <dialaashna@gmail.com>
2020-07-23 16:01:39 +0000

    #6

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 
-print("welcome to the trivia game \nI ask you questions and you see if you can answer them")
+print("Welcome to trivia! \nAll you have to answer the questions I ask you \nlet's see how much you know about the NBA")
 def main():
-	ask = input("do you think your ready?: ")
+	ask = input("\ndo you think your ready?: ")
 	if ask != "yes":
 		print("\nI guess your not up for the challenge :(\ncome back when your ready!")
 		return 0
@@ -10,12 +10,10 @@ def main():
 		print("LETS GET STARTED!")
 
 	correctanswers = 0
-	player = -1
 
 	#1)
-	question1 = input("\nwhat is the capitol of massachusetts?: ")
-	if question1 != "boston":
-		gameover = True
+	question1 = input("\nWho has the most missed shots in the NBA? \na)LeBron James \nb)Kobe Bryant \nc)Steph Curry \n:")
+	if question1 != "b" and question1 != "B":
 		print("wrong answer :(")
 		correctanswers +=0
 
@@ -25,8 +23,7 @@ def main():
 
 	#2)
 	question2 = input("\nWhat NBA team has won the most chapionships?: ")
-	if question2 != "celtics" :
-		gameover = True
+	if question2 != "celtics" and question2 != "boston" and question2 != "boston celtics":
 		print("wrong answer :( ")
 		correctanswers +=0
 
@@ -36,8 +33,7 @@ def main():
 
 	#3)
 	question3 = input("\nhow many teams are in the NBA?: ")
-	if question3 != "30":
-		gameover = True
+	if question3 != "30" and question3 != "30 teams":
 		print("wrong answer :( ")
 		correctanswers +=0
 
@@ -48,7 +44,6 @@ def main():
 	#4)
 	question4 = input("\nwho had the longest NBA career?: ")
 	if question4 != "vince carter":
-		gameover = True
 		print("wrong answer :( ")
 		correctanswers +=0
 
@@ -57,9 +52,8 @@ def main():
 		correctanswers +=1
 
 	#5
-	question5 = input ("\nWhat tem won the 2019 NBA championship?: ")
-	if question5 != "raptors":
-		gameover = True
+	question5 = input("\nWhat tem won the 2019 NBA championship?: ")
+	if question5 != "raptors" and question5 != "toronto" and question5 != "toronto raptors":
 		print("wrong answer :( ")
 		correctanswers +=0
 	
@@ -67,6 +61,14 @@ def main():
 		print("good job!")
 		correctanswers +=1
 
+	#6
+	question6 = input("\nWho has the record for the most 3's in a game? \na)Klay Thompson \nb)Steph Curry \nc)Kevin Durant \n:")
+	if question6 != "a" and question6 != "A":
+		print("wrong answer :( ")
+		correctanswers +=0
+	else:
+		print("good job!")
+		correctanswers +=1
 
-	print(f"\nyou got {correctanswers}/5 right")
+	print(f"\nyou got {correctanswers}/6 right")
 main()
